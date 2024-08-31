2f5c4545a47aa232a69c0db65fd42c629aa828f7
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-08-31 16:38:57 -0400

    add columns in files and cids tables

--- priv/repo/migrations/20240831140627_create_files.exs
+++ priv/repo/migrations/20240831140627_create_files.exs
@@ -4,7 +4,12 @@ defmodule Fileonchain.Repo.Migrations.CreateFiles do
   def change do
     create table(:files) do
       add :filename, :text
-      add :data, :bytes
+      add :data, :text
+      add :mime_type, :text
+      add :size, :integer
+      add :created_by, references(:users, on_delete: :nothing), null: false
+      add :created_at, :utc_datetime
+      add :deleted_at, :utc_datetime
 
       timestamps(type: :utc_datetime)
     end
--- priv/repo/migrations/20240831140942_create_cids.exs
+++ priv/repo/migrations/20240831140942_create_cids.exs
@@ -5,6 +5,14 @@ defmodule Fileonchain.Repo.Migrations.CreateCids do
     create table(:cids) do
       add :cid, :string
       add :data, :text
+      add :tx_hash, :text
+      add :tx_index, :integer
+      add :tx_block, :integer
+      add :tx_timestamp, :integer
+      add :tx_from, :text
+      add :created_by, references(:users, on_delete: :nothing), null: false
+      add :created_at, :utc_datetime
+      add :deleted_at, :utc_datetime
 
       timestamps(type: :utc_datetime)
     end
