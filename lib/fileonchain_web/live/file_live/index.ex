8a6ee682469fc87b8a2e64aa0b9053bb38b6bde7
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-08-31 15:53:06 -0400

    add image preview for png

--- lib/fileonchain_web/live/file_live/index.ex
+++ lib/fileonchain_web/live/file_live/index.ex
@@ -38,4 +38,20 @@ defmodule FileonchainWeb.FileLive.Index do
 
     {:noreply, stream_delete(socket, :files, file)}
   end
+
+  # Updated function to render file preview
+  def render_file_preview(file) do
+    if is_png?(file.data) do
+      "<img src=\"data:image/png;base64,#{file.data}\" alt=\"#{file.filename}\" style=\"max-width: 100px; max-height: 100px;\" />"
+    else
+      "<span class=\"text-gray-500\">Preview not available</span>"
+    end
+  end
+
+  defp is_png?(data) do
+    case Base.decode64(data) do
+      {:ok, decoded} -> String.starts_with?(decoded, <<137, 80, 78, 71, 13, 10, 26, 10>>)
+      _ -> false
+    end
+  end
 end
