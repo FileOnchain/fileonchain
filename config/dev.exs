ea7acb5116b5ebf5bfa406da7c5414d5bc4ebd0a
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-08-31 15:33:56 -0400

    add docker and improve layout

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :fileonchain, Fileonchain.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "fileonchain_dev",
+  database: "fileonchain",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
--- lib/fileonchain_web/components/core_components.ex
+++ lib/fileonchain_web/components/core_components.ex
@@ -486,27 +486,27 @@ defmodule FileonchainWeb.CoreComponents do
         <tbody
           id={@id}
           phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
-          class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
+          class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-white"
         >
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50">
+          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50 hover:text-black">
             <td
               :for={{col, i} <- Enum.with_index(@col)}
               phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0", @row_click && "hover:cursor-pointer"]}
+              class={["relative p-0 text-white", @row_click && "hover:cursor-pointer text-gray"]}
             >
               <div class="block py-4 pr-6">
-                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
-                <span class={["relative", i == 0 && "font-semibold text-zinc-900"]}>
+                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 hover:text-black sm:rounded-l-xl" />
+                <span class={["relative", i == 0 && "font-semibold text-white"]}>
                   <%= render_slot(col, @row_item.(row)) %>
                 </span>
               </div>
             </td>
-            <td :if={@action != []} class="relative w-14 p-0">
+            <td :if={@action != []} class="relative w-14 p-0 text-white">
               <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
+                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 hover:text-black sm:rounded-r-xl" />
                 <span
                   :for={action <- @action}
-                  class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
+                  class="relative ml-4 font-semibold leading-6 text-white hover:text-black"
                 >
                   <%= render_slot(action, @row_item.(row)) %>
                 </span>
