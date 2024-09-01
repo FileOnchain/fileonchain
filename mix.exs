0e271c495b547be172b4d813aa44a23b7b24655e
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-09-01 06:26:51 -0400

    try improving js setup

--- mix.exs
+++ mix.exs
@@ -61,7 +61,7 @@ defmodule Fileonchain.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:blake3, "~> 1.0"}
+      {:blake3, "~> 1.0.0"}
     ]
   end
 
